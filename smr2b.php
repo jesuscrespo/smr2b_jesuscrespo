d62c60e62a863bae6f0d2a80c2c7a4dd2becfae5
jesuscrespo <jesuscrespo280@gmail.com>
2019-10-01 18:10:41 +0200

    suscriptores

--- smr2b.php
+++ smr2b.php
@@ -1,11 +1,26 @@
 <a href="http://192.168.0.105/smr2b_cesar/smr2b.php">cesar</a>
 jesus
-<a href="http://192.168.0.139/smr2b_david/smr2b.php">campo</a>
+<a href="http://192.168.0.130/smr2b_david/smr2b.php">campo</a>
+
+
+<form>
+	<input name="email" type="email">
+	<button>Enviar</button>
+</form>
 
 <?php
-$f=fopen('visitas.txt','a');
+$f=fopen('visitas.txt' , 'a');
 fwrite($f,date('Y-m-d H:i:s '));
-fwrite($f,$_SERVER['REMOTE_ADDR']);
-fwrite($f,"\r\n");
-fclose($f)
+fwrite ($f,$_SERVER['REMOTE_ADDR']);
+fwrite ($f, "\r\n");
+fclose($f);
+?>
+
+<?php
+if(isset($_GET['email'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']);
+	fwrite($f,"\r\n");
+	fclose($f);
+}
 ?>
