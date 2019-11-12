389365735b1f64d6730d124d299a093786b11a92
Jesús Crespo <55751714+jesuscrespo@users.noreply.github.com>
2019-11-12 18:23:02 +0100

    Create html.php

---
+++ html.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title>JESUS CRESPO</title>
+</head>
+<body>
+<font face="arialblack, calibri">
+	<h1>CUETO</h1>
+	<font color="blue">Cueto, es un pueblo situado en el norte de santander</font>
+	<br/>
+	<h2>Mi lugar preferido</h2>
+	<font color="green">Jesus Crespo<b>Mi lugar preferido</b></font>
+	<br/>
+	<h3>Consta de 4 lugares para el baño de agua salada</h3>
+	<u>Antuerta</u>,La cantera, El bocal, Molinucos y mataleñas
+	<font size=5>Playa de mataleñas</font>
+	<br><br/>
+	
+	<center>
+	<table border>
+	<tr><td>autonomia</td><td>provincia</td></tr>
+	<tr><td>Galicia</td><td>LaCoruña</td></tr>
+	<tr><td>Galicia</td><td>Ourense</td></tr>
+	<tr><td>Cantabria</td><td>Cantabria</td></tr>
+	<tr><td>Asturias</td><td>Asturias</td></tr>
+	<tr><td>Pais Vasco</td><td>Gipuzkoa</td></tr>
+	</table>	
+	</center>
+	
+	<form>
+	
+	<input type="text">
+	<input type="submit">
+</font>
+</body>
+</html>
