407bf4c942cd365dd0843d83de766a14bf1313cd
Jesús Crespo <55751714+jesuscrespo@users.noreply.github.com>
2019-11-26 19:09:46 +0100

    Update triangulosb.php

--- triangulosb.php
+++ triangulosb.php
@@ -1,19 +1,19 @@
 <?php
 
-function tipo_angulos($a,$b,$c){
-	$r='acutangulo';
+function tipo_ángulos($a,$b,$c){
+	$r='acutángulo';
 	
 	if($a==90 or $b==90 or $c==90)
-		$r='rectangulo';
+		$r='rectángulo';
 	
 	if ($a>90 or $b>90 or $c>90)
-		$r='obtusangulo';
+		$r='obtusángulo';
 	
 	return $r;
 }	
-echo tipo_angulos(90,50,40).' ¿rectangulo?<br>';
-echo tipo_angulos(80,60,40).' ¿acutangulo?<br>';
-echo tipo_angulos(100,30,50).' ¿obstusangulo?<br>';
-echo tipo_angulos(50,60,70).' ¿acutangulo?<br>';
-echo tipo_angulos(90,80,10).' ¿rectangulo?<br>';
-echo tipo_angulos(40,20,120).' ¿obtusangulo?<br>';
+echo tipo_angulos(90,50,40).' ¿rectángulo?<br>';
+echo tipo_angulos(80,60,40).' ¿acutángulo?<br>';
+echo tipo_angulos(100,30,50).' ¿obstusángulo?<br>';
+echo tipo_angulos(50,60,70).' ¿acutángulo?<br>';
+echo tipo_angulos(90,80,10).' ¿rectángulo?<br>';
+echo tipo_angulos(40,20,120).' ¿obtusángulo?<br>';
