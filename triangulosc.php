d0ebb20b4a7a69e9ba10d406bc61a3fccec03f7d
Jesús Crespo <55751714+jesuscrespo@users.noreply.github.com>
2019-11-26 19:15:14 +0100

    Update triangulosc.php

--- triangulosc.php
+++ triangulosc.php
@@ -1,22 +1,21 @@
 <?php
 
+function tipo_angulos($A,$B,$C){
+	$r='acutángulo';
+	
+	if($A==90 or $B==90 or $C==90)
+		$r='rectángulo';
+	
+	if ($A>90 or $B>90 or $C>90)
+		$r='obtusángulo';
+	return $r;
+}
 function tipo_lados($a,$b,$c){
 	$r='escaleno';
 	if($a==$b or $b==$c or $a==$c)
 		$r='isoceles';
 	if($a==$b and $b==$c)
-		$r='equilatero';
-	return $r;
-}
-function tipo_angulos($A,$B,$C){
-	$r='acutangulo';
-	
-	if($A==90 or $B==90 or $C==90)
-		$r='rectangulo';
-	
-	if ($A>90 or $B>90 or $C>90)
-		$r='obtusangulo';
-	
+		$r='equilátero';
 	return $r;
 }
 function triangulos ($a,$b,$c,$A,$B,$C){
@@ -25,8 +24,8 @@ function triangulos ($a,$b,$c,$A,$B,$C){
 	
 	return $r1.' - '.$r2;
 }
-echo triangulos (1,1,1,60,60,60).' ¿equilatero? ¿acutangulo? <br>';
-echo triangulos (1,1,2,45,45,90).' ¿isoceles? ¿rectangulo? <br>';
-echo triangulos (2,3,1,110,35,35).' ¿escaleno? ¿obstusangulo? <br>';
-echo triangulos (2,1,1,45,90,45).' ¿isoceles? ¿rectangulo? <br>';
-echo triangulos (3,2,1,35,110,35).' ¿escaleno? ¿obstusangulo? <br>';
+echo triangulos (1,1,1,60,60,60).' ¿equilátero? ¿acutángulo? <br>';
+echo triangulos (1,1,2,45,45,90).' ¿isóceles? ¿rectángulo? <br>';
+echo triangulos (2,3,1,110,35,35).' ¿escaleno? ¿obstusángulo? <br>';
+echo triangulos (2,1,1,45,90,45).' ¿isoceles? ¿rectángulo? <br>';
+echo triangulos (3,2,1,35,110,35).' ¿escaleno? ¿obstusángulo? <br>';
